42ba21ff6d49a79b39c9399a4669c55d0509e664
Adam Blakey <adam@blakey.family>
2025-07-05 18:02:26 +0100

    - Limit the number of messages to 1000. - Improved logout when user_id invalid. - Fixed base path for dev.

--- RealtimeChat/Services/ChatService.cs
+++ RealtimeChat/Services/ChatService.cs
@@ -5,11 +5,13 @@ namespace RealtimeChat.Services;
 
 public class ChatService {
     private readonly List<Chat> _chats = [];
-    
+    private int MAX_CHAT_COUNT = 1000; 
+
     public event Action? UpdateHandler;
 
     public void Add(User user, string message)
     {
+        if (_chats.Count >= MAX_CHAT_COUNT) _chats.RemoveAt(0); // Remove the oldest chat if the limit is reached
         _chats.Add(new Chat(user, message));
         UpdateHandler?.Invoke();
     }
