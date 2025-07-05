860a2ba0cd08644a1023a3145081155efc261d1b
Adam Blakey <adam@blakey.family>
2025-07-05 13:51:15 +0100

    - Added profanity filter. - Added update handlers. - Added text box and submit button.

--- RealtimeChat/Services/ChatService.cs
+++ RealtimeChat/Services/ChatService.cs
@@ -1,12 +1,27 @@
-﻿using MoreLinq;
+﻿using Microsoft.AspNetCore.Components;
+using MoreLinq;
 using RealtimeChat.Models;
 
 namespace RealtimeChat.Services;
 
 public class ChatService {
     private readonly List<Chat> _chats = [];
+    private readonly ProfanityFilter.ProfanityFilter _profanityFilter = new();
+    public event Action? UpdateHandler;
 
-    public void Add(User user, string message) => _chats.Add(new Chat(user, message));
+    public void Add(User user, string message)
+    {
+        _chats.Add(new Chat(user, message));
+        UpdateHandler?.Invoke();
+    }
 
     public IEnumerable<IGrouping<string, Chat>> GroupedChats => _chats.GroupAdjacent(c => c.UserId);
+
+    public bool ContainsProfanity(string message) => _profanityFilter.DetectAllProfanities(message).Any();
+
+    public void Clear()
+    {
+        _chats.Clear();
+        UpdateHandler?.Invoke();
+    }
 }
