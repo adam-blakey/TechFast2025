cf568fd1478199044c8c739a269dddaab64ae125
Adam Blakey <adam@blakey.family>
2025-07-12 00:09:19 +0100

    Kind of a working-ish game. Still need to:
    - Work on initializing the characters.
    - Add additional icons.
    - Un-hardcode the icon index.
    - Add animations for movement.
    - SVG-ify the icons for niceness.

---
+++ Game/Hubs/CharacterHub.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+public class CharacterHub : Hub
+{
+    public async Task UpdatePosition(string userId, Game.Models.CharacterState state)
+    {
+        await Clients.All.SendAsync("ReceivePosition", userId, state);
+    }
+}
---
+++ Game/Models/CharacterState.cs
@@ -0,0 +1,9 @@
+﻿namespace Game.Models
+{
+    public class CharacterState
+    {
+        public int IconId { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
